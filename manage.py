82eca9fafd95ec8e34b28e51c78ca51100930061
agustin <agustin.gomez.mansilla@gmail.com>
2023-02-04 00:09:44 +0100

    Added license message (amend)

--- manage.py
+++ manage.py
@@ -1,11 +1,4 @@
 #!/usr/bin/env python
-"""
-Propietario: grIT
-Contacto: agustin.gomez.mansilla@gmail.com
-
-Use of this code for any commercial purpose is NOT AUTHORIZED.
-El uso de éste código para cualquier propósito comercial NO ESTÁ AUTORIZADO.
-"""
 import os
 import sys
 
