7765239e174c458acfeb6016a978f245cdd52661
agustin <agustin.gomez.mansilla@gmail.com>
2023-02-20 21:04:58 +0100

    quitar pandas

--- eventos/utils.py
+++
@@ -1,8 +0,0 @@
-#TODO: deberiamos usar algo mas liviano que pandas para hacer el import
-import pandas
-
-def read_client_list(fn):
-    df = pandas.read_excel(fn)
-    # Find the first row of the table and set it as headers.
-
-    return df
