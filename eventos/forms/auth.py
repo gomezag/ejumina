77a083ab1cc04c1d5b9073fade7ca42d57f7abe6
agustin <agustin.gomez.mansilla@gmail.com>
2023-02-25 17:16:08 +0100

    New User form

--- eventos/forms/auth.py
+++ eventos/forms/auth.py
@@ -1,17 +1,31 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UsernameField
-from django.forms import TextInput, CharField, PasswordInput
+from django.contrib.auth.models import Group
+import django.forms as forms
+from eventos.models import Usuario
+
 
 class UserLoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super(UserLoginForm, self).__init__(*args, **kwargs)
 
     username = UsernameField(
-        widget=TextInput(attrs={'autofocus': True}),
+        widget=forms.TextInput(attrs={'autofocus': True}),
         label="Usuario",
     )
-    password = CharField(
+    password = forms.CharField(
         label= "Contraseña",
         strip=False,
-        widget=PasswordInput(attrs={'autocomplete': 'current-password'}),
+        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password'}),
     )
+
+
+class NewUserForm(forms.ModelForm):
+    class Meta:
+        model = Usuario
+        fields = ['username', 'password', 'groups', 'first_name', 'email']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['groups'].queryset = Group.objects.exclude(name='superuser')
+        self.fields['password'].widget = forms.PasswordInput()
