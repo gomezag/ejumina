c050f54fa2f8ee8ead675a6d2663299acbe12405
agustin <agustin.gomez.mansilla@gmail.com>
2023-03-09 17:39:10 +0100

    Usuario e entrada puede revisar usuarios. Cada usuario puede ver cuantos checkins tiene en el panel evento.

--- eventos/views/usuarios.py
+++ eventos/views/usuarios.py
@@ -13,11 +13,15 @@ from smtplib import SMTPRecipientsRefused
 
 from eventos.forms import NewUserForm, FreeAssignToUserForm, EditUserForm
 from eventos.models import Usuario, Evento
-from .basic_view import AdminView
+from .basic_view import BasicView
+from eventos.utils import validate_in_group
 
 
-class PanelUsuario(AdminView):
+class PanelUsuario(BasicView):
     template_name = 'eventos/panel_usuario.html'
+
+    def test_func(self):
+        return self.request.user.is_authenticated and validate_in_group(self.request.user, ('admin', 'entrada'))
 
     def get_context_data(self, user, *args, **kwargs):
         c = super().get_context_data(user)
@@ -43,7 +47,8 @@ class PanelUsuario(AdminView):
     def get(self, request, *args, **kwargs):
         user = request.user
         c = self.get_context_data(user, *args, **kwargs)
-        c['form'] = FreeAssignToUserForm()
+        if validate_in_group(user, ('admin',)):
+            c['form'] = FreeAssignToUserForm()
 
         return super().get(request, c)
 
@@ -51,16 +56,21 @@ class PanelUsuario(AdminView):
         id_vendedor = kwargs.get('id_usuario')
         vendedor = Usuario.objects.get(id=id_vendedor)
         user = request.user
-        form = FreeAssignToUserForm(request.POST)
-        if form.is_valid():
-            form.save(user, vendedor)
+        if validate_in_group(user, ('admin',)):
+            form = FreeAssignToUserForm(request.POST)
+            if form.is_valid():
+                form.save(user, vendedor)
         c = self.get_context_data(request.user, *args, **kwargs)
-        c['form'] = form
+        if form:
+            c['form'] = form
         return super().get(request, c)
 
 
-class ListaUsuarios(AdminView):
+class ListaUsuarios(BasicView):
     template_name = 'eventos/lista_usuarios.html'
+
+    def test_func(self):
+        return self.request.user.is_authenticated and validate_in_group(self.request.user, ('admin', 'entrada'))
 
     def get_context_data(self, user, *args, **kwargs):
         c = super().get_context_data(user)
@@ -69,59 +79,63 @@ class ListaUsuarios(AdminView):
 
     def get(self, request, *args, **kwargs):
         c = self.get_context_data(request.user)
-        c['form'] = NewUserForm()
-        c['edit_form'] = EditUserForm(auto_id='edit')
+        if validate_in_group(request.user, ('admin',)):
+            c['form'] = NewUserForm()
+            c['edit_form'] = EditUserForm(auto_id='edit')
         return super().get(request, c)
 
     def post(self, request, *args, **kwargs):
-        delete = request.POST.get('delete', None)
-        reactivate = request.POST.get('reactivate', None)
-        reset = request.POST.get('reset', None)
-        edit = request.POST.get('edit', None)
-        msg = []
-        if delete:
-            user = Usuario.objects.get(pk=delete)
-            user.is_active = False
-            user.save()
-            form = NewUserForm()
-            edit_form = EditUserForm()
-        elif reactivate:
-            user = Usuario.objects.get(pk=reactivate)
-            user.is_active = True
-            user.save()
-            form = NewUserForm()
-            edit_form = EditUserForm()
-        elif edit:
-            user = Usuario.objects.get(pk=edit)
-            edit_form = EditUserForm(request.POST, instance=user)
-            if edit_form.is_valid():
-                edit_form.save()
-            form = NewUserForm()
-        elif reset:
-            target = Usuario.objects.get(pk=reset)
-            pwd_form = PasswordResetForm(data={
-                'email': target.email
-            })
-            if pwd_form.is_valid():
-                try:
-                    pwd_form.save(
-                        request=request,
-                        use_https=True,
-                        email_template_name='registration/password_reset_email.html'
-                    )
-                    msg = 'Mail enviado!'
-                except SMTPRecipientsRefused:
-                    msg.append('La dirección de email fue rechazada por el servidor.')
-                    msg.append('Verificá la dirección de correo e intentá de vuelta.')
-            form = NewUserForm()
-            edit_form = EditUserForm()
+        if validate_in_group(request.user, ('admin',)):
+            delete = request.POST.get('delete', None)
+            reactivate = request.POST.get('reactivate', None)
+            reset = request.POST.get('reset', None)
+            edit = request.POST.get('edit', None)
+            msg = []
+            if delete:
+                user = Usuario.objects.get(pk=delete)
+                user.is_active = False
+                user.save()
+                form = NewUserForm()
+                edit_form = EditUserForm()
+            elif reactivate:
+                user = Usuario.objects.get(pk=reactivate)
+                user.is_active = True
+                user.save()
+                form = NewUserForm()
+                edit_form = EditUserForm()
+            elif edit:
+                user = Usuario.objects.get(pk=edit)
+                edit_form = EditUserForm(request.POST, instance=user)
+                if edit_form.is_valid():
+                    edit_form.save()
+                form = NewUserForm()
+            elif reset:
+                target = Usuario.objects.get(pk=reset)
+                pwd_form = PasswordResetForm(data={
+                    'email': target.email
+                })
+                if pwd_form.is_valid():
+                    try:
+                        pwd_form.save(
+                            request=request,
+                            use_https=True,
+                            email_template_name='registration/password_reset_email.html'
+                        )
+                        msg = 'Mail enviado!'
+                    except SMTPRecipientsRefused:
+                        msg.append('La dirección de email fue rechazada por el servidor.')
+                        msg.append('Verificá la dirección de correo e intentá de vuelta.')
+                form = NewUserForm()
+                edit_form = EditUserForm()
+            else:
+                form = NewUserForm(request.POST)
+                if form.is_valid():
+                    form.save()
+                edit_form = EditUserForm()
+            c = self.get_context_data(request.user)
+            c['form'] = form
+            c['edit_form'] = edit_form
+            c['alert_msg'] = msg
         else:
-            form = NewUserForm(request.POST)
-            if form.is_valid():
-                form.save()
-            edit_form = EditUserForm()
-        c = self.get_context_data(request.user)
-        c['form'] = form
-        c['edit_form'] = edit_form
-        c['alert_msg'] = msg
+            c = self.get_context_data(request.user)
         return render(request, self.template_name, context=c)
