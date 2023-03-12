7a685e8abf5f4304866bb95c25212401c3ff3a29
agustin <agustin.gomez.mansilla@gmail.com>
2023-03-12 09:57:33 +0100

    Arreglar urls

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,6 @@ from api.views import *
 urlpatterns = [
     path('user/login', Login.as_view()),
     path('user/login', Logout.as_view()),
-    path('evento/upcoming', Eventos.as_view({'get': 'upcoming'})),
-    path('evento/all', Eventos.as_view({'get': 'list'}))
+    path('evento/ongoing', Eventos.as_view({'get': 'ongoing'})),
+    path('evento/all', Eventos.as_view({'get': 'all'}))
 ]
