cef529bb45906426e43fa24b7df539cc6c0b7e4b
agustin <agustin.gomez.mansilla@gmail.com>
2023-09-10 11:17:39 +0200

    Add csrf error view

---
+++ eventos/views/errors.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+
+def csrf_fail(request, reason=""):
+    c = dict(reason=reason)
+    return render(request, 'errors/csrf_fail.html', c)
